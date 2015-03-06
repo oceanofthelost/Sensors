ade8b5dea739dc5b4d3730739b83adee3d0dff86
Sean Alling <sean.alling@gmail.com>
2015-03-05 22:03:21 -0800

    Fixed the header file. Updated the .c file

--- MAG3110/DriverMAG3110.c
+++ MAG3110/DriverMAG3110.c
@@ -1,9 +1,9 @@
 #include <stdint.h>
 #include "DriverMAG3110.h"
 
-extern uint8_t MagWriteByte			(uint8_t,uint8_t);
-extern uint8_t MagWriteMultipleBytes(uint8_t,uint8_t *,uint8_t);
-extern uint8_t MagReadByte			(uint8_t,uint8_t *);
+extern uint8_t MagWriteByte		(uint8_t,uint8_t);
+extern uint8_t MagWriteMultipleBytes	(uint8_t,uint8_t *,uint8_t);
+extern uint8_t MagReadByte		(uint8_t,uint8_t *);
 extern uint8_t MagReadMultipleByte	(uint8_t,uint8_t *,uint8_t);
 
 static uint8_t SetReg1(uint8_t mask, uint8_t constant)
@@ -20,7 +20,7 @@ static uint8_t SetReg1(uint8_t mask, uint8_t constant)
 static uint8_t SetReg2(uint8_t mask, uint8_t constant)
 {
 	uint8_t currentReg2Flags;
-	ReadCtrlReg1(&currentReg2Flags);
+	ReadCtrlReg2(&currentReg2Flags);
 
 	currentReg2Flags &= constant;
 	currentReg2Flags |= mask; 
@@ -42,61 +42,40 @@ uint8_t SetOverSampleAndDataRate(uint8_t CONSTANT)
 
 uint8_t ReadMagData(int16_t *readPtr)
 {
-    uint8_t readBuffer[6] = {0};
-    uint8_t *readLocalPtr = readBuffer;
+	uint8_t readBuffer[6] = {0};
+	uint8_t *readLocalPtr = readBuffer;
 
-    MagReadMultipleByte(OUT_X_MSB, readLocalPtr,6);
-    for (uint8_t i = 0; i <= 2; ++i)
-    {
-    	readPtr[i] = (readBuffer[i * 2] << 8) | readBuffer[i * 2 + 1];
-    }
+ 	MagReadMultipleByte(OUT_X_MSB, readLocalPtr,6);
+	for (uint8_t i = 0; i <= 2; ++i)
+	{
+ 		readPtr[i] = (readBuffer[i * 2] << 8) | readBuffer[i * 2 + 1];
+	}
 
-    return 0;
+	return 0;
 }
 
 uint8_t WriteOffsetCorrection(uint16_t *offsetData)
 {
+	uint8_t writeData[6] = {0};
+	uint8_t *writePtr = writeData;
 
-    uint8_t writeData[6] = {0};
-    uint8_t *writePtr = writeData;
-
-    for (uint8_t i = 0; i < 3; i++)
-    {
-        writeData[i] = offsetData[i] >> 8;
-        writeData[i + 1] = offsetData[i] << 8;
-    }
-    
-    return MagWriteMultipleBytes(OFF_X_MSB, writePtr, 6);
-}
-
-static uint8_t AutoResetToggle(uint8_t mask)
-{
-	return SetReg2(mask,0x7F);
-}
-
-static uint8_t ToggleData(uint8_t mask)
-{
-	return SetReg2(mask,0xF7);
-}
-
-static uint8_t ReadModeToggle(uint8_t mask)
-{
-	return SetReg1(mask,0xFB);
-}
-
-static uint8_t SetMode(uint8_t mask)
-{
-	return SetReg1(mask,0xFC);
+	for (uint8_t i = 0; i < 3; i++)
+	{
+		writeData[i] = offsetData[i] >> 8;
+	        	writeData[i + 1] = offsetData[i] << 8;
+	}
+	    
+	return MagWriteMultipleBytes(OFF_X_MSB, writePtr, 6);
 }
 
 uint8_t WriteCtrlReg1(uint8_t data)
 {
-    return MagWriteByte(CTRL_REG1, data);
+	return MagWriteByte(CTRL_REG1, data);
 }
 
 uint8_t WriteCtrlReg2(uint8_t data)
 {
-    return MagWriteByte(CTRL_REG2, data);
+	return MagWriteByte(CTRL_REG2, data);
 }
 
 uint8_t SetCtrlReg1Default()
@@ -111,51 +90,51 @@ uint8_t SetCtrlReg2Default()
 
 uint8_t SetContinuousMode()
 {
-	return SetMode(CTRL_REG1_CONTINUOUS);
+	return SetReg1(CTRL_REG1_CONTINUOUS,0xFC);
 }
 
 uint8_t SetStandbyMode()
 {
-	return SetMode(CTRL_REG1_STANDBY);
+	return SetReg1(CTRL_REG1_STANDBY,0xFC);
 }
 
 uint8_t SetSingleMeasurmentMode()
 { 
-	return SetMode(CTRL_REG1_SINGLE_MEASURMENT);
+	return SetReg1(CTRL_REG1_SINGLE_MEASURMENT,0xFC);
 }
 
 uint8_t SetTriggerMeasurmentMode()
 {
-	return SetMode(CTRL_REG1_TRIGGER_MEASURMENT);
+	return SetReg1(CTRL_REG1_TRIGGER_MEASURMENT,0xFC);
 }
 
 uint8_t SetFastReadOn()
 {	
-	return ReadModeToggle(CTRL_REG1_FAST_READ_EN);
+	return SetReg1(CTRL_REG1_FAST_READ_EN,0xFB);
 }
 
 uint8_t SetFastReadOff()
 {
-	return ReadModeToggle(CTRL_REG1_FAST_READ_NENEN);
+	return SetReg1(CTRL_REG1_FAST_READ_NEN,0xFB);
 }
 
 uint8_t SetAutoResetOn()
 {
-	return  AutoResetToggle(CTRL_REG2_AUTO_MRST_EN);
+	return SetReg2(CTRL_REG2_AUTO_MRST_EN,0x7F);
 }
-uint8_t SetAutoResetOff	()
+uint8_t SetAutoResetOff()
 {
-	return  AutoResetToggle(CTRL_REG2_AUTO_MRST_NENEN);
+	return SetReg2(CTRL_REG2_AUTO_MRST_NEN,0x7F);
 }
 
 uint8_t SetUserCorrectedData()
 {
-	return ToggleData( CTRL_REG2_RAW_NEN);
+	return SetReg2(CTRL_REG2_RAW_NEN,0xF7);
 }
 
 uint8_t SetRawData()
 {
-	return ToggleData( CTRL_REG2_RAW_EN);
+	return SetReg2(CTRL_REG2_RAW_EN,0xF7);	
 }
 
 uint8_t ResetMag()
@@ -165,30 +144,30 @@ uint8_t ResetMag()
 
 uint8_t ReadDrStatus(uint8_t *readPtr)
 {
-    return MagReadByte(DR_STATUS, readPtr);
+	return MagReadByte(DR_STATUS, readPtr);
 }
 
 uint8_t ReadWhoAmI(uint8_t *readPtr)
 { 
-    return MagReadByte(WHO_AM_I, readPtr);
+	return MagReadByte(WHO_AM_I, readPtr);
 }
 
 uint8_t ReadSysMod(uint8_t *readPtr)
 {
-    return MagReadByte(SYSMOD, readPtr);
+	return MagReadByte(SYSMOD, readPtr);
 }
 
 int8_t ReadDieTep(int8_t *readPtr)
 {
-    return MagReadByte(DIE_TEMP, readPtr);
+	return MagReadByte(DIE_TEMP, readPtr);
 }
 
 uint8_t ReadCtrlReg1(uint8_t *readPtr)
 {
-    return MagReadByte(CTRL_REG1, readPtr);
+	return MagReadByte(CTRL_REG1, readPtr);
 }
 
 uint8_t ReadCtrlReg2(uint8_t *readPtr)
 {
-    return MagReadByte(CTRL_REG2, readPtr);
+	return MagReadByte(CTRL_REG2, readPtr);
 }
--- MAG3110/DriverMAG3110.h
+++ MAG3110/DriverMAG3110.h
@@ -51,56 +51,6 @@
 
     All follow the format above.
 */
-<<<<<<< HEAD
-#define CTRL_REG1_8000_016_1280_9000_040		0x00
-#define CTRL_REG1_4000_032_1280_9000_035 	0x08
-#define CTRL_REG1_2000_064_1280_9000_030        	0x10
-#define CTRL_REG1_1000_128_1280_9000_025        	0x18
-
-#define CTRL_REG1_4000_016_0640_5500_040    	0x20
-#define CTRL_REG1_2000_032_0640_5500_035    	0x28
-#define CTRL_REG1_1000_064_0640_5500_030 	0x30
-#define CTRL_REG1_0500_128_0640_5500_025		0x38
-
-#define CTRL_REG1_2000_016_0320_2750_040    	0x40
-#define CTRL_REG1_1000_032_0320_2750_035    	0x48
-#define CTRL_REG1_0500_064_0320_2750_030    	0x50
-#define CTRL_REG1_0250_128_0320_2750_025    	0x58
-
-#define CTRL_REG1_1000_016_0128_1375_040    	0x60
-#define CTRL_REG1_0500_032_0128_1375_035    	0x68
-#define CTRL_REG1_0250_064_0128_1375_030    	0x70
-#define CTRL_REG1_0125_128_0128_1375_025    	0x78
-
-#define CTRL_REG1_0500_016_0080_0688_040    	0x80
-#define CTRL_REG1_0250_032_0080_0688_035    	0x88
-#define CTRL_REG1_0125_064_0080_0688_030    	0x90
-#define CTRL_REG1_0063_128_0080_0688_025    	0x98
-
-#define CTRL_REG1_0250_016_0080_0344_040        	0xA0
-#define CTRL_REG1_0125_032_0080_0344_035        	0xA8
-#define CTRL_REG1_0063_064_0080_0344_030        	0xB0
-#define CTRL_REG1_0031_128_0080_0344_025        	0xB8
-
-#define CTRL_REG1_0125_016_0080_0172_040        	0xC0
-#define CTRL_REG1_0063_032_0080_0172_035        	0xC8
-#define CTRL_REG1_0031_064_0080_0172_030        	0xD0
-#define CTRL_REG1_0016_128_0080_0172_025        	0xD8
-
-#define CTRL_REG1_0063_016_0080_0086_040        	0xE0
-#define CTRL_REG1_0031_032_0080_0086_035        	0xE8
-#define CTRL_REG1_0016_064_0080_0086_030        	0xF0
-#define CTRL_REG1_0008_128_0080_0086_025        	0xF8
-
-#define CTRL_REG1_FAST_READ_EN           		0x04
-#define CTRL_REG1_FAST_READ_NEN       		0x00
-
-#define CTRL_REG1_DEFAULT                       		0x00
-#define CTRL_REG1_STANDBY                       		0x00
-#define CTRL_REG1_CONTINUOUS                    		0x01
-#define CTRL_REG1_SINGLE_MEASURMENT         	0x02
-#define CTRL_REG1_TRIGGER_MEASURMENT            	0x03
-=======
 #define CTRL_REG1_8000_016_1280_9000_040	0x00
 #define CTRL_REG1_4000_032_1280_9000_035	0x08
 #define CTRL_REG1_2000_064_1280_9000_030	0x10
@@ -149,8 +99,6 @@
 #define CTRL_REG1_CONTINUOUS			0x01
 #define CTRL_REG1_SINGLE_MEASURMENT		0x02
 #define CTRL_REG1_TRIGGER_MEASURMENT		0x03
->>>>>>> origin/master
-
 
 /* Control Register 2 */
 /*  When implementing functions that alter this register
@@ -168,26 +116,6 @@
 
 /* Function Prototypes */
 /* Write Functions */
-<<<<<<< HEAD
-uint8_t WriteOffSetCorrection   		(uint16_t *);
-uint8_t WriteCtrlReg1           		(uint8_t);
-uint8_t WriteCtrlReg2           		(uint8_t);
-uint8_t SetOverSampleAndDataRate	(uint8_t);
-uint8_t SetStandbyMode          		();
-uint8_t SetCtrlReg1Default      		();
-uint8_t SetContinuousMode       		();
-uint8_t SetSingleMeasurmentMode 	();
-uint8_t SetTriggerMeasurmentMode	();
-uint8_t SetFastReadOn           		();
-uint8_t SetFastReadOff          		();
-uint8_t SetCtrlReg2Default      		();
-uint8_t SetAutoResetOn          		();
-uint8_t SetAutoResetOff         		();
-uint8_t SetUserCorrectedData    	();
-uint8_t SetRawData              		();
-uint8_t ResetMag                		();
-
-=======
 uint8_t WriteOffSetCorrection	(uint16_t *);
 uint8_t WriteCtrlReg1		(uint8_t);
 uint8_t WriteCtrlReg2		(uint8_t);
@@ -207,7 +135,6 @@ uint8_t SetAutoResetOff		();
 uint8_t SetUserCorrectedData	();
 uint8_t SetRawData		();
 uint8_t ResetMag		();
->>>>>>> origin/master
 
 /* Read Functions */
 uint8_t ReadDrStatus    	(uint8_t *);
